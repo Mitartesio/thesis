cab40d10ff5c6eafe8dbbef8a8f7098c898f7ef3
frek <frek@itu.dk>
2025-08-28 14:53:18 +0200

    Simple Test Init

---
+++ SimpleTest.java
@@ -0,0 +1,34 @@
+public class SimpleTest {
+
+    static int a = 0;
+    static int b = 0;
+    static int x = 0;
+    static int y = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000000; i++) {
+            a = b = x = y = 0;
+
+            Thread t1 = new Thread(() -> {
+                a = 1;
+                x = b;
+            });
+
+            Thread t2 = new Thread(() -> {
+                b = 1;
+                y = a;
+            });
+
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+
+            if (x == 0 && y == 0) {
+                System.out.println("Found unwanted interleaving: x=0, y=0 at iteration "+i);
+                break;
+            }
+        }
+    }
+}
