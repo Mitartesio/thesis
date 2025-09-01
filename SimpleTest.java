61dc955fedc79d8c618b4740ba91cf7760251e57
frek <frek@itu.dk>
2025-09-01 14:59:14 +0200

    added gitignore for idea and class files. Added SimpleTest.jpf

--- SimpleTest.java
+++ SimpleTest.java
@@ -6,7 +6,9 @@ public class SimpleTest {
     static int y = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 1; i++) {
+
+
             a = b = x = y = 0;
 
             Thread t1 = new Thread(() -> {
@@ -25,10 +27,14 @@ public class SimpleTest {
             t1.join();
             t2.join();
 
-            if (x == 0 && y == 0) {
-                System.out.println("Found unwanted interleaving: x=0, y=0 at iteration "+i);
-                break;
-            }
+//             if (x == 0 && y == 0) {
+//                 System.out.println("Found unwanted interleaving: x=0, y=0 at iteration " + i);
+//                 break;
+//             }
+
+
+            assert x == 1 && y == 1 : "Interleaving exposes a bug where x = " + x + " and y = " +y;
+
         }
     }
 }
