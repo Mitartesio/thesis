4eb072f7874cfffff168dda3d1766df32a4bcf36
= <=>
2025-09-12 10:33:09 +0200

    Random scheduler with input/outpur for found result implemented

--- Listeners/Random_Scehduler.java
+++ Listeners/Random_Scehduler.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import gov.nasa.jpf.PropertyListenerAdapter;
 import gov.nasa.jpf.search.Search;
 import gov.nasa.jpf.vm.ChoiceGenerator;
+import gov.nasa.jpf.vm.MJIEnv;
 import gov.nasa.jpf.vm.ThreadChoiceGenerator;
 import gov.nasa.jpf.vm.ThreadInfo;
 import gov.nasa.jpf.vm.VM;
@@ -13,10 +14,19 @@ import gov.nasa.jpf.vm.VM;
 
 public class Random_Scehduler extends PropertyListenerAdapter {
     private Random random = new Random();
+    private boolean found = false;
+    private int valToFInd = 2;
 
     @Override
     public void choiceGeneratorAdvanced(VM vm, ChoiceGenerator<?> cg) {
         // Check if cg is actully a ThreadChoiceGenerator
+
+        ThreadInfo ti = vm.getCurrentThread();
+        MJIEnv env = ti.getEnv();
+        if (env.getStaticIntField("SimpleTest2", "answer") == valToFInd) {
+            found = true;
+        }
+
         if (cg instanceof ThreadChoiceGenerator) {
             // Convert to ThreadChoiceGenerator to use getTotalNumberOfChoices method
             ThreadChoiceGenerator tcg = (ThreadChoiceGenerator) cg;
@@ -26,14 +36,16 @@ public class Random_Scehduler extends PropertyListenerAdapter {
             // random choice
             int choice = random.nextInt(numberOfThreads);
 
+            // Uncomment this section for debugging: Will show which thread is chosen at
+            // each point!
+
             // Get all threads
-            Object[] objs = tcg.getAllChoices();
+            // Object[] objs = tcg.getAllChoices();
 
             // Cast to ThreadInfo
-            ThreadInfo chosenThread = (ThreadInfo) objs[choice];
+            // ThreadInfo chosenThread = (ThreadInfo) objs[choice];
 
-            // Print thread for debugging
-            System.out.println(chosenThread);
+            // System.out.println(chosenThread);
 
             // Randomly choose the next thread
             tcg.select(choice);
@@ -45,7 +57,20 @@ public class Random_Scehduler extends PropertyListenerAdapter {
     // terminate
     @Override
     public void stateAdvanced(Search search) {
+        // If value has been found
+        if (found) {
+            System.out.println("1");
+            search.terminate();
+        }
+        // If search is at end state we check if the current field of x == value to find
+        // to print 1 or 0 and the terminate
         if (search.isEndState()) {
+            if (found || search.getVM().getCurrentThread().getEnv().getStaticIntField("SimpleTest2",
+                    "answer") == valToFInd) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
             search.terminate();
         }
     }
