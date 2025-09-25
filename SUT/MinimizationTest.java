01c53ca657ca7290fa7ddf57582f5bd88791fc48
ANMV <ANMV@itu.dk>
2025-09-25 17:45:49 +0200

    Implemented mini-test and corresponding jpf file. Error found correctly.

--- SUT/MinimizationTest.java
+++ SUT/MinimizationTest.java
@@ -1,43 +1,45 @@
 package SUT;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MinimizationTest {
-    static int x = 0, y = 0, z = 0; //, w = 0;
-    
+    static int x = 0, y = 0, z = 0; // , w = 0;
+    private static boolean conditionMet = false;
+
     public static void main(String[] args) throws InterruptedException {
+
         ReentrantLock lock = new ReentrantLock();
         Condition w = lock.newCondition();
 
-        Thread Ta = new Thread(() -> {
-
-            lock.lock();
-            try {
-                x++;
-                y++;
-                w.signal();
-                assert (z < 5);
-            } finally {
-                lock.unlock();
-            }
-        }, "Ta");
-
         Thread Tb = new Thread(() -> {
             int a = 0, b = 0;
+            x = 1;
+            a = x;
+            y = a;
             lock.lock();
             try {
-                x = 1;
-                a = x;
-                y = a;
-                w.await();
+                while (!conditionMet) {
+                    w.await();
+                }
                 b = y;
                 z = a + b;
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                e.printStackTrace();
             } finally {
                 lock.unlock();
             }
         }, "Tb");
+
+        Thread Ta = new Thread(() -> {
+            lock.lock();
+            x++;
+            y++;
+            conditionMet = true;
+            w.signal();
+            lock.unlock();
+            assert (z < 5) : "I found the error where !(z < 5)";
+        }, "Ta");
 
         Ta.start();
         Tb.start();
