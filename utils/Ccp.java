a18ebddcb85b7b6da658a631ee16ddc4e17897e0
ANMV <ANMV@itu.dk>
2025-09-26 15:20:00 +0200

    ArrayExample implemented and corresponding jpf file

--- utils/Ccp.java
+++ utils/Ccp.java
@@ -14,7 +14,7 @@ public class Ccp {
                 sum += Math.pow((1 - P[i]), k);
             }
             k += 1;
-            System.out.println("sum: " + sum + " eps: " + eps);
+            // System.out.println("sum: " + sum + " eps: " + eps);
 
         }
         return k - 1;
@@ -23,13 +23,12 @@ public class Ccp {
     public static void main(String[] args) {
         Ccp myCcp = new Ccp();
 
-        double[] arr = new double[2000];
+        System.out.println((double) 1 / 6);
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) 1 / 2000;
-        }
+        double[] arr = new double[] { 0.16666666666666666, 0.16666666666666666, 0.16666666666666666,
+                0.16666666666666666, 0.16666666666666666, 0.16666666666666666 };
 
-        System.out.println(myCcp.calcCcp(2000, arr, 0.1));
+        System.out.println(myCcp.calcCcp(6, arr, 0.05));
     }
 
 }
