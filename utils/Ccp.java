fde516f6c831f558308b575e69b574b7a00beb00
ANMV <ANMV@itu.dk>
2025-09-29 10:06:10 +0200

    Ccp without main

--- utils/Ccp.java
+++ utils/Ccp.java
@@ -13,22 +13,11 @@ public class Ccp {
             for (int i = 0; i < P.length; i++) {
                 sum += Math.pow((1 - P[i]), k);
             }
+
             k += 1;
-            // System.out.println("sum: " + sum + " eps: " + eps);
 
         }
         return k - 1;
-    }
-
-    public static void main(String[] args) {
-        Ccp myCcp = new Ccp();
-
-        System.out.println((double) 1 / 6);
-
-        double[] arr = new double[] { 0.16666666666666666, 0.16666666666666666, 0.16666666666666666,
-                0.16666666666666666, 0.16666666666666666, 0.16666666666666666 };
-
-        System.out.println(myCcp.calcCcp(6, arr, 0.05));
     }
 
 }
