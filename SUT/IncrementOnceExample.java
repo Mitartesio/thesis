373564b7fad9f18a6f748cd226b6af6708663e67
frek <frek@itu.dk>
2025-10-08 15:50:03 +0200

    FEAT: added IncrementOnceExample and IncrementOnceExample.jpf

---
+++ SUT/IncrementOnceExample.java
@@ -0,0 +1,24 @@
+package SUT;
+
+
+public class IncrementOnceExample {
+    static int x;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            x++;
+        }, "T1");
+
+        Thread t2 = new Thread(() -> {
+            x++;
+        }, "T2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+
+    }
+}
