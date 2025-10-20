3a9caecd6d2f00b4f26d97c5cecc25a533014358
ANMV <ANMV@itu.dk>
2025-10-20 12:28:01 +0200

    Listener_Uniform_Adapts.java finished and ready for test

--- Listeners/Listener_Uniform_Adapts.java
+++ Listeners/Listener_Uniform_Adapts.java
@@ -3,6 +3,7 @@ package Listeners;
 import gov.nasa.jpf.vm.ThreadInfo;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.PropertyListenerAdapter;
@@ -25,6 +26,7 @@ public class Listener_Uniform_Adapts extends PropertyListenerAdapter {
     private boolean initializedAll;
     private String nextThread;
     private int numberOfThreads;
+    private Random random;
 
     public Listener_Uniform_Adapts(Config config) {
         this.config = config;
@@ -35,27 +37,34 @@ public class Listener_Uniform_Adapts extends PropertyListenerAdapter {
         // this.numberOfThreads =
         // Integer.parseInt(config.getString("Listener_Uniform_Adapts.numberOfThreads").strip());
         System.out.println("Number of threads is: " + numberOfThreads);
+        this.random = new Random();
     }
 
-    // The purpose of initConfig is that when an end state has been reached it will
-    // reinitialized the listener for a new iteration
-    private void initConfig() {
+    // This method will called from the listener it is attached to every time
+    // it reaches an end point or is unable to continue search
+    public void searchRestart() {
         System.out.println("Config");
         if (initializedAll) {
-            this.liveThreads = new HashMap<>();
             for (String string : threadsAndOperations.keySet()) {
                 liveThreads.put(string, threadsAndOperations.get(string));
             }
+
         } else {
             numberOfThreads--;
             nextThread = null;
             if (numberOfThreads <= 0) {
                 initializedAll = true;
+                // Debugging
                 System.out.println("Succesfully initialized!!!");
+                System.out.println("THis is the size: " + threadsAndOperations.size());
                 for (String string : threadsAndOperations.keySet()) {
                     System.out.println("Thread: " + string + " number of ops: " + threadsAndOperations.get(string));
                 }
-                initConfig();
+                // Debugging
+                this.liveThreads = new HashMap<>();
+                for (String string : threadsAndOperations.keySet()) {
+                    liveThreads.put(string, threadsAndOperations.get(string));
+                }
             }
 
         }
@@ -72,85 +81,94 @@ public class Listener_Uniform_Adapts extends PropertyListenerAdapter {
     // threadsAndOperations for the next iteration
     @Override
     public void choiceGeneratorAdvanced(VM vm, ChoiceGenerator<?> cg) {
-        if (vm.getSearch().isEndState()) {
-            System.out.println("Found the endState here for some reason");
-        }
-        if (initializedAll) {
-            System.out.println("I should probably do something else here");
-        } else {
-            if (cg instanceof ThreadChoiceGenerator) {
-                ThreadChoiceGenerator tcg = (ThreadChoiceGenerator) cg;
+        if (cg instanceof ThreadChoiceGenerator) {
+            ThreadChoiceGenerator tcg = (ThreadChoiceGenerator) cg;
+            if (initializedAll) {
+                int sum = 0;
+
+                for (String thread : liveThreads.keySet()) {
+                    sum += liveThreads.get(thread);
+                }
+
+                int acc = random.nextInt(sum) + 1;
+
+                sum = 0;
+                String chosenThread = "";
+                for (String thread : liveThreads.keySet()) {
+                    sum += liveThreads.get(thread);
+                    if (sum >= acc) {
+                        chosenThread = thread;
+                        break;
+                    }
+                }
+
+                System.out.println("This is the chosen thread: " + chosenThread + " this is the acc: " + acc);
 
                 Object[] chosenThreads = tcg.getAllChoices();
-
-                int choice = -1;
-
-                // System.out.println("NextThread is: " + nextThread);
-
-                if (vm.getSearch().isEndState() || !vm.getSearch().hasNextState()) {
-                    System.out.println("End State here");
+                if (numberOfThreads <= 0 || chosenThreads.length == 1) {
+                    for (int i = 0; i < chosenThreads.length; i++) {
+                        ThreadInfo ti = (ThreadInfo) chosenThreads[i];
+                        if (ti.getName().equals("main")) {
+                            tcg.select(i);
+                            return;
+                        }
+                    }
                 }
 
-                // Loop through all available threads and put them into threadsAndOperations.
-                // This way we will keep track of all live threads throughout the program
                 for (int i = 0; i < chosenThreads.length; i++) {
                     ThreadInfo ti = (ThreadInfo) chosenThreads[i];
-                    // System.out.println(ti.toString());
-
-                    if (!ti.getName().equals("main")) {
-                        threadsAndOperations.putIfAbsent(ti.getName(), 0);
-                    }
-
-                    // Insofar
-                    if (nextThread == null && !ti.getName().equals("main")
-                            && threadsAndOperations.get(ti.getName()) == 0) {
-                        nextThread = ti.getName();
-                    }
-
-                    if (ti.getName().equals(nextThread)) {
-                        System.out.println(ti.getPC().toString());
-                        choice = i;
-                        threadsAndOperations.put(nextThread, threadsAndOperations.get(nextThread) + 1);
+                    if (ti.getName().equals(chosenThread)) {
+                        int val = liveThreads.get(chosenThread);
+                        if (val > 1) {
+                            liveThreads.put(chosenThread, val - 1);
+                        }
+                        tcg.select(i);
+                        break;
                     }
                 }
-                // System.out.println("This many ops: " + threadsAndOperations.get(nextThread));
-                // System.out.println("Choice is: " + choice);
-                if (choice >= 0) {
-                    tcg.select(choice);
+            } else {
+                if (cg instanceof ThreadChoiceGenerator) {
+
+                    Object[] chosenThreads = tcg.getAllChoices();
+
+                    int choice = -1;
+
+                    // System.out.println("NextThread is: " + nextThread);
+
+                    if (vm.getSearch().isEndState() || !vm.getSearch().hasNextState()) {
+                        // System.out.println("End State here");
+                    }
+
+                    // Loop through all available threads and put them into threadsAndOperations.
+                    // This way we will keep track of all live threads throughout the program
+                    for (int i = 0; i < chosenThreads.length; i++) {
+                        ThreadInfo ti = (ThreadInfo) chosenThreads[i];
+                        // System.out.println(ti.toString());
+
+                        if (!ti.getName().equals("main")) {
+                            threadsAndOperations.putIfAbsent(ti.getName(), 0);
+                        }
+
+                        // Insofar
+                        if (nextThread == null && !ti.getName().equals("main")
+                                && threadsAndOperations.get(ti.getName()) == 0) {
+                            nextThread = ti.getName();
+                        }
+
+                        if (ti.getName().equals(nextThread)) {
+                            // System.out.println(ti.getPC().toString());
+                            choice = i;
+                            threadsAndOperations.put(nextThread, threadsAndOperations.get(nextThread) + 1);
+                        }
+                    }
+                    // System.out.println("This many ops: " + threadsAndOperations.get(nextThread));
+                    // System.out.println("Choice is: " + choice);
+                    if (choice >= 0) {
+                        tcg.select(choice);
+                    }
                 }
             }
         }
-    }
-
-    // StateAdvanced has two responsibilities:
-    // 1. If !initializedAll then it will backTrack insofar it reaches an end state
-    // and switch to another thread to explore
-    // 2. If initializedAll it will restart the listener with InitConfig
-    @Override
-    public void stateAdvanced(Search search) {
-        // if (search.isEndState()) {
-        // System.out.println("Found end state");
-        // if (!initializedAll) {
-        // System.out.println("Minus");
-        // numberOfThreads--;
-        // System.out.println("End point with the thread: " + nextThread);
-        // if (numberOfThreads <= 0) {
-        // initializedAll = true;
-        // System.out.println("I am initialized");
-
-        // for (String string : threadsAndOperations.keySet()) {
-        // System.out.println("This is the key: " + string + " this is the number of
-        // ops: "
-        // + threadsAndOperations.get(string));
-        // }
-        // }
-        // }
-        // initConfig();
-        // }
-    }
-
-    public void searchRestart() {
-        initConfig();
     }
 
 }
--- SUT/TestTest.java
+++ SUT/TestTest.java
@@ -3,16 +3,45 @@ package SUT;
 public class TestTest {
     static int x;
 
-    public static void main(String[] args) {
-        Thread t1 = new Thread(() -> x++, "t1");
-        Thread t2 = new Thread(() -> x++, "t2");
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            x++;
+            x++;
+            x++;
+            x++;
+            x++;
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            x++;
+            x++;
+            x++;
+            x++;
+            x++;
+            x++;
+            x++;
+        }, "t2");
+
+        Thread t3 = new Thread(() -> {
+            x++;
+        }, "t3");
+
+        Thread t4 = new Thread(() -> {
+            x++;
+            x++;
+            x++;
+        }, "t4");
+
         t1.start();
         t2.start();
-        try {
-            t1.join();
-            t2.join();
-        } catch (Exception e) {
-        }
-        assert x > 1 : "not good";
+        t3.start();
+        t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+        System.out.println("The result of x is: " + x);
     }
 }
