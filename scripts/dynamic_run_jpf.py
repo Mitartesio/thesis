fe91dbc1be7558b86b9e1e01e1de49dd92a49ea2
Mitartesio <tobiaslysdal@gmail.com>
2025-11-11 10:55:36 +0100

    update python file to have combine csv func

--- scripts/dynamic_run_jpf.py
+++ scripts/dynamic_run_jpf.py
@@ -1,7 +1,9 @@
 import csv
 import os, pathlib, sys, subprocess
+from pathlib import Path
 from datetime import datetime
 from typing import List, Tuple
+import pandas as pd
 
 # Fixed path
 
@@ -92,6 +94,21 @@ def populate_csv(csv_name: str, answers: List[int]):
             writer.writerow([problem, k, viol])
 
     print(f" answers -> {out_file.stem}.csv")
+
+
+def combine_and_convert_csv(
+    csv1: str, csv2: str, combinedname: str):
+    csv1_path = ROOT / "reports" / f"{csv1}.csv"
+    csv2_path = ROOT / "reports" / f"{csv2}.csv"
+
+    output_path = ROOT / "reports" / f"{combinedname}.csv"
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    csv1 = pd.read_csv(csv1_path)
+    csv2 = pd.read_csv(csv2_path)
+    csv1_df = pd.DataFrame(csv1)
+    csv2_df = pd.DataFrame(csv2)
+    combined_csv = pd.concat([csv1_df, csv2_df], ignore_index=True)
+    return combined_csv.to_csv(output_path, index=False, float_format="%.0f")
 
 
 def handle_jpf(): #uses cmd line args, otherwise utilizes the dictionary of algo to jpf
@@ -213,16 +230,6 @@ algo_to_jpf = {
 
 
 if __name__ == "__main__":
-
-
-    # from pathlib import Path
-    #
-    # JAVA = os.environ.get("JAVA_HOME")
-    # JAVA = str(Path(JAVA) / "bin" / "java") if JAVA else "java"
-    #
-    # # Cehck which java we're running from
-    # print("[debug] using java:", JAVA)
-
 
     # use args when calling file to get it to run the experiment you're trying to.
     # if no args provided, utilizes the algo_to_jpf dictionary
