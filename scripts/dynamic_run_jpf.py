f965b352d01f57bb076b916de999a177e4c97d78
Mitartesio <tobiaslysdal@gmail.com>
2025-12-03 15:00:24 +0100

    deleted a duplicate function from dynamic run

--- scripts/dynamic_run_jpf.py
+++ scripts/dynamic_run_jpf.py
@@ -94,21 +94,6 @@ def populate_csv(csv_name: str, answers: List[int]):
     print(f" answers -> {out_file.stem}.csv")
 
 
-def combine_and_convert_csv(
-    csv1: str, csv2: str, combinedname: str):
-    csv1_path = ROOT / "reports" / f"{csv1}.csv"
-    csv2_path = ROOT / "reports" / f"{csv2}.csv"
-
-    output_path = ROOT / "reports" / f"{combinedname}.csv"
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-    csv1 = pd.read_csv(csv1_path)
-    csv2 = pd.read_csv(csv2_path)
-    csv1_df = pd.DataFrame(csv1)
-    csv2_df = pd.DataFrame(csv2)
-    combined_csv = pd.concat([csv1_df, csv2_df], ignore_index=True)
-    return combined_csv.to_csv(output_path, index=False, float_format="%.0f")
-
-
 def handle_jpf(): #uses cmd line args, otherwise utilizes the dictionary of algo to jpf
     # sys.arg[0] python file to run, sys.arg[1] jpf.config, sys.arg[2] amount runs
 
